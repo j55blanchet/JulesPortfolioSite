7e92ac965f996ea32c949742876013ac69f568bf
Julien Blanchet <j55blanchet@gmail.com>
2018-12-17 21:38:49 -0500

    Slideshow Improvements

--- projects/teardrop/teardrop.js
+++ projects/teardrop/teardrop.js
@@ -7,7 +7,7 @@ let loadImageIfNeeded = function(index) {
     }
     
     if (!imageElems[index].src) {
-        console.log("Loading src for index " + index)
+        // console.log("Loading src for index " + index)
         imageElems[index].src = imageElems[index].dataset.src;
     }
 }
@@ -16,24 +16,74 @@ let loadImageIfNeeded = function(index) {
 let slides = [].slice.call(document.querySelectorAll('#teardrop-slideshow .slide'));
 let currentSlide = 0;
 
-let nextSlide = function() {
-    setSlide((currentSlide+1)%slides.length);
+let nextSlide = () => {
+    setSlide((currentSlide + 1) % slides.length);
 }
 
-let slideInterval = setInterval(nextSlide, 2000);
+let slideTimeMs = 2000;
+let slideInterval = setInterval(nextSlide, slideTimeMs);
+
+let scheduleSlideAdvance = () => {
+    slideInterval = setInterval(nextSlide, slideTimeMs);
+};
 
 let setSlide = function(index) {
     slides[currentSlide].className = 'slide';
-    currentSlide = index;
+    currentSlide = +index;
     slides[currentSlide].className = 'slide showing';
     loadImageIfNeeded(currentSlide);
-    loadImageIfNeeded(currentSlide + 1 % slides.length);
+    loadImageIfNeeded((currentSlide + 1) % slides.length);
     document.querySelector('input.slideshowSlider').value = currentSlide;
+
+    console.log("setSlide: Switched to slide " + index);
+
+    clearInterval(slideInterval);
+    if (!isPaused) {
+        scheduleSlideAdvance();
+    }
+}
+
+let isPaused = false;
+let pause = function() {
+    clearInterval(slideInterval);
+    isPaused = true;
+}
+let resume = function() {
+    scheduleSlideAdvance();
+    isPaused = false;
+}
+
+let pauseButton = document.querySelector('.playpause');
+pauseButton.onclick = (e) => {
+    
+    console.log("PlayPause clicked", e);
+    if (isPaused) {
+        pauseButton.innerHTML = `<div class="icon is-medium"><i class="fas fa-fw fa-lg fa-pause"></i></div>`;
+        resume();
+        return;
+    } else {
+        pauseButton.innerHTML = `<div class="icon is-medium"><i class="fas fa-fw fa-lg fa-play"></i></div>`;
+        pause();
+        return;
+    }
+}
+
+document.querySelector('#teardrop-slideshow .left').onclick = (e) => {
+    console.log("Move left clicked");
+    let nSlide = currentSlide - 1;
+    if (nSlide < 0) { nSlide = slides.length - 1;}
+    setSlide(nSlide);
+}
+
+document.querySelector('#teardrop-slideshow .right').onclick = (e) => {
+    console.log("Move right clicked")
+    nextSlide();
 }
 
 document.querySelector('input.slideshowSlider').oninput = (e) => {
 
     if (e.target.value !== currentSlide) {
+        console.log("Slider: switching to: " + e.target.value);
         setSlide(e.target.value);
     }
 }
